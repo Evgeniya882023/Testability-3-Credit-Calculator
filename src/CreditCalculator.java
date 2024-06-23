8898fd0ccb4f92ec31db583d8fb79cdcb43dec31
Evgeniya Malashenko <malashenko_genya@mail.ru>
2024-06-23 21:49:06 +0300

    second commit - PercentRate move to class with service method

--- src/CreditCalculator.java
+++ src/CreditCalculator.java
@@ -1,5 +1,6 @@
 public class CreditCalculator {
-    public double calculate(double percentRate, double sum, double month) {
+    public double calculate(double percent, double sum, double month) {
+        double percentRate = percent / 12 / 100;
         return sum * (percentRate * Math.pow((1 + percentRate), month)) / (Math.pow((1 + percentRate), month) - 1);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,8 @@ public class Main {
         CreditCalculator service = new CreditCalculator();
         double sum = 1000000;
         double percent = 9.99;
-        double percentRate = percent / 12 / 100;
         double month = 12;
-        double monthPayment = service.calculate(percentRate, sum, month);
+        double monthPayment = service.calculate(percent, sum, month);
         int monthPaymentResult = (int) monthPayment;
         // при кредите 1 млн должно получиться 87911 при взятии кредита на год, 46140 - на два года, 32262 - на три года
         System.out.println(monthPaymentResult);
